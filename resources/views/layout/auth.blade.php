5675eb61334621f35a1d27a6f9233a538999930a
Camille Kamimura <camille.kamimura@wemakecodes.com>
2021-02-17 13:25:05 -0300

    MECHANIX-50 feat: criando layout para autenticação

--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -12,24 +12,31 @@
 		<script src="https://kit.fontawesome.com/72fbff17f3.js" crossorigin="anonymous"></script>
 
 		<link href="{{ asset('img/favicon.ico/') }}" rel="shortcut icon">
-		<!-- <link rel="stylesheet" href="{{ asset('assets/css/owl-carousel-min.css/') }}">
-		<link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.min.css/') }}"> -->
 		<link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600&display=swap" rel="stylesheet">
-		<link href="{{ asset('assets/css/style.css/') }}?v=3" rel="stylesheet" type="text/css">
+		
+		<link href="{{ asset('assets/css/auth.css/') }}?v=3" rel="stylesheet" type="text/css"> 
 		<link href="{{ asset('assets/css/includes.css/') }}?v=3" rel="stylesheet" type="text/css"> 
 
 	</head>
 	<body>
-		@include('includes._header')
-	    @yield('content')
+		@include('includes._simple-header')
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-6 d-none d-md-block d-lg-block d-xl-block px-0">
+						<img src="{{url('/')}}/images/car-mechanic-and-customer-shaking-hands.jpg" alt="login image" class="login-img"> 
+					</div>    
+					
+					<div class="col-md-6 col-sm-12 container-auth px-5 pt-5 pb-4">
+						@yield('content-auth')
+					</div>
+				</div>
+			</div>
+
 	    @include('includes._footer')
      	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<!-- <script src="{{ asset('assets/js/modernizr.js/') }}" type="text/javascript"></script> -->
-		<!-- <script src="{{ asset('assets/js/owl.carousel.min.js/') }}" type="text/javascript"></script> -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 		@yield('script')
-				
 	</body>
 </html>
