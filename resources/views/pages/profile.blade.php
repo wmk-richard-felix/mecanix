fb660204b53fd23f495cfe7c86a060fdf9e6aee0
camille-kamimura <camille.kamimura@wemakecodes.com>
2021-03-03 23:47:43 -0300

    MECHANIX-32 feat: adiciona esqueleto

---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.master')
+
+@section('title', 'Alterar meu perfil')
+@section('description', 'O maior portal de profissionais de veículos do Brasil')
+
+@section('content')
+	<section class="section-category mt-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 box-category">
+					<h1>Alterar meu perfil</h1>
+                </div>
+			</div>
+		</div>
+	</section>	 
+@endsection
+
+@section('script')
+@endsection
